4fa33fe5e88b5b665dac7b0784f920b96b4b196c
kitakou0313 <kitakou0313@gmail.com>
2022-04-22 11:05:24 +0900

    Implyment script to sync nginx.conf

--- deploy/sync-settings.sh
+++ deploy/sync-settings.sh
@@ -1,2 +1,26 @@
+#!/bin/bash
+set -e
 # This scripts is used to 
 # - sync setting(nginx , mysql, systemd, etc...) to remote host.
+
+echo "Start to sync config files"
+
+isu01="localhost"
+
+trghosts=($isu01)
+
+for host in "${trghosts[@]}"
+do
+  echo "Send configs to ${host}"
+  rsync -e "ssh -p 2222" -av ../kataribe/webserver-config/nginx.conf root@${host}:/etc/nginx/nginx.conf
+done
+
+# for server in isu01 isu02 isu03; do
+#     ssh -t $server "sudo systemctl stop isucon.golang.service"
+#     scp ./app $server:/home/isucon/webapp/go/isucon
+#     # templateは別途rsyncする必要がある
+#     rsync -av ./src/isucon/views/ $server:/home/isucon/webapp/go/src/isucon/views/
+#     ssh -t $server "sudo systemctl start isucon.golang.service"
+# done
+
+echo "finish deploy ${USER}"
