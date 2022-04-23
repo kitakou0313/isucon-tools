daa00e6192eb7d518037961b1c46157475869f98
kitakou0313 <kitakou0313@gmail.com>
2022-04-23 13:56:27 +0900

    Implyment script to fetch log file

--- bench/after-bench.sh
+++ bench/after-bench.sh
@@ -1,2 +1,18 @@
+#!/bin/bash
+set -e
 # This scripts is used to 
 # - fetch log files(nginx, mysql logs) to analyze in local.
+
+echo "Start to fetch log file from localhost"
+
+isu01="localhost"
+
+trghosts=($isu01)
+
+for host in "${trghosts[@]}"
+do
+  echo "Fetch log from ${host}"
+  rsync -e "ssh -p 2222" -av root@${host}:/var/log/nginx/access.log ./kataribe/webserver-log/access.log
+done
+
+echo "Finish to fetch log files"
