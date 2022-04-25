32b86f0392bc9903f4459a9ab9bdaf44f811d134
kitakou0313 <kitakou0313@gmail.com>
2022-04-25 10:12:59 +0900

    Change a way to backup log

--- bench/backup-log.sh
+++ bench/backup-log.sh
@@ -1,4 +1,4 @@
 if [ -f /var/log/nginx/access.log ]; then 
-    mv /var/log/nginx/access.log /var/log/nginx/access.log.bk.$(date "+%Y%m%d_%H%M%S")
-    touch /var/log/nginx/access.log
+    cp /var/log/nginx/access.log /var/log/nginx/access.log.bk.$(date "+%Y%m%d_%H%M%S")
+    cat /dev/null > /var/log/nginx/access.log
 fi
