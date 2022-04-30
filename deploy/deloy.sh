7b1918e97b1f343a88e12c5a1478b5dd4fbf4ef9
kitakou0313 <kitakou0313@gmail.com>
2022-04-30 11:28:33 +0900

    Implyment scripts to deploy test

--- deploy/deloy.sh
+++ deploy/deloy.sh
@@ -1,3 +1,16 @@
-# Build web app
+#!/bin/bash
+set -e
+# This scripts is used to 
+# - send bin and init SQL files to host with rsync
 
-# Send bin and init SQL files to host with rsync
+echo "Start to deploy apps"
+
+source ./hosts/hosts.txt
+
+for host in "${trghosts[@]}"
+do
+  echo "Deploy to ${host}"
+  rsync -e "ssh -p 2222" -av ./webapp/sample-webapp/sample root@${host}:/home/root/webapp/sample/src
+done
+
+echo "Finish to deploy apps"
