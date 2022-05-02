4c810c2d16c2fa1b1ee71d9eeb016af7a5ce495f
kitakou0313 <kitakou0313@gmail.com>
2022-05-02 10:52:12 +0900

    Add scrpits to fetch mysql log

--- bench/after-bench.sh
+++ bench/after-bench.sh
@@ -12,12 +12,15 @@ for host in "${trghosts[@]}"
 do
   echo "Fetch log from ${host}"
   rsync -e "ssh -p 2222" -av root@${host}:/var/log/nginx/access.log ./kataribe/webserver-log/access.log
+
+  # Fetch mysql slowquery-log
+  echo "Fetch mysql slow query log from ${host}"
+  rsync -e "ssh -p 2222" -av root@${host}:/var/log/mysql/mysql-slow.log ./mysql-slowquery/mysql-slowquery-log/mysql-slow.log
   
   # Fetch pprof logs
   echo "Fetch pprof log from ${host}"
   rsync -e "ssh -p 2222" -av root@${host}:/home/root/webapp/cpu.pprof ./pprof/profilefiles/cpu.pprof
 
-  # Fetch mysql slowquery-log
 done
 
 echo "Finish to fetch log files"
