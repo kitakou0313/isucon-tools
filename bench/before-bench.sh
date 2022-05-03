ae6e1ee7817671537568235c2bc9c2ce71ad7738
kitakou0313 <kitakou0313@gmail.com>
2022-05-03 10:58:07 +0900

    Change shell to exec script in remote host

--- bench/before-bench.sh
+++ bench/before-bench.sh
@@ -13,8 +13,8 @@ HOSTS_NUM=${#trghosts[@]}
 for ((host_idx=0; host_idx<${HOSTS_NUM}; host_idx++));
 do
   echo "Backup log and clean log in ${trghosts[host_idx]}:${trgports[host_idx]}"
-  ssh -p ${trgports[host_idx]} root@${trghosts[host_idx]} 'sh -s ' < ./bench/utils/nginx-backup-clean-log.sh
-  ssh -p ${trgports[host_idx]} root@${trghosts[host_idx]} 'sh -s ' < ./bench/utils/slowquery-backup-clean-log.sh
+  ssh -p ${trgports[host_idx]} root@${trghosts[host_idx]} 'bash -s ' < ./bench/utils/nginx-backup-clean-log.sh
+  ssh -p ${trgports[host_idx]} root@${trghosts[host_idx]} 'bash -s ' < ./bench/utils/slowquery-backup-clean-log.sh
 done
 
 echo "Finish to backup log and clean log"
