5248308d95e307de546a78e3d593a4c55e1e127a
kitakou0313 <kitakou0313@gmail.com>
2022-05-06 09:02:16 +0900

    Bundle configs to one dir

--- deploy/sync-settings.sh
+++ deploy/sync-settings.sh
@@ -11,8 +11,8 @@ HOSTS_NUM=${#trghosts[@]}
 for ((host_idx=0; host_idx<${HOSTS_NUM}; host_idx++));
 do
   echo "Send configs to ${trghosts[host_idx]}:${trgports[host_idx]}"
-  rsync -e "ssh -p ${trgports[host_idx]}" -av ./kataribe/webserver-config/nginx.conf root@${trghosts[host_idx]}:/etc/nginx/nginx.conf
-  rsync -e "ssh -p ${trgports[host_idx]}" -av ./mysql-slowquery/mysql-config/my.cnf root@${trghosts[host_idx]}:/etc/mysql/my.cnf
+  rsync -e "ssh -p ${trgports[host_idx]}" -av ./configs/nginx.conf root@${trghosts[host_idx]}:/etc/nginx/nginx.conf
+  rsync -e "ssh -p ${trgports[host_idx]}" -av ./configs/my.cnf root@${trghosts[host_idx]}:/etc/mysql/my.cnf
 done
 
 echo "Finish to sync config files"
