4586422392dd591179ffdb0e7c87cba16f084a37
kitakou0313 <kitakou0313@gmail.com>
2022-05-12 10:53:12 +0900

    Enable config to change username and pub key

--- bench/after-bench.sh
+++ bench/after-bench.sh
@@ -11,21 +11,21 @@ source ./hosts/hosts.txt
 for ((host_idx=0; host_idx<${FRONTEND_HOSTS_NUMS}; host_idx++));
 do
   echo "Fetch log from ${FRONTEND_HOSTS[host_idx]}:${FRONTEND_HOSTS_SSH_PORT[host_idx]}"
-  rsync -e "ssh -p ${FRONTEND_HOSTS_SSH_PORT[host_idx]}" -av root@${FRONTEND_HOSTS[host_idx]}:/var/log/nginx/access.log ./kataribe/webserver-log/access.log
+  rsync -e "ssh -p ${FRONTEND_HOSTS_SSH_PORT[host_idx]} -i ${FRONTEND_HOSTS_SSH_PUB_KEY[host_idx]}" -av ${FRONTEND_HOSTS_SSH_USER}@${FRONTEND_HOSTS[host_idx]}:/var/log/nginx/access.log ./kataribe/webserver-log/access.log
 done
 
 for ((host_idx=0; host_idx<${DB_HOSTS_NUMS}; host_idx++));
 do
   # Fetch mysql slowquery-log
   echo "Fetch mysql slow query log from ${DB_HOSTS[host_idx]}:${DB_HOSTS_SSH_PORT[host_idx]}"
-  rsync -e "ssh -p ${DB_HOSTS_SSH_PORT[host_idx]}" -av root@${DB_HOSTS[host_idx]}:/var/log/mysql/mysql-slow.log ./mysql-slowquery/mysql-slowquery-log/mysql-slow.log
+  rsync -e "ssh -p ${DB_HOSTS_SSH_PORT[host_idx]} -i ${DB_HOSTS_SSH_PUB_KEY[host_idx]}" -av ${DB_HOSTS_SSH_USER[host_idx]}@${DB_HOSTS[host_idx]}:/var/log/mysql/mysql-slow.log ./mysql-slowquery/mysql-slowquery-log/mysql-slow.log
 done
 
 for ((host_idx=0; host_idx<${APP_HOSTS_NUMS}; host_idx++));
 do 
   # Fetch pprof logs
   echo "Fetch pprof log from ${APP_HOSTS[host_idx]}:${APP_HOSTS_SSH_PORT[host_idx]}"
-  rsync -e "ssh -p ${APP_HOSTS_SSH_PORT[host_idx]}" -av root@${APP_HOSTS[host_idx]}:/home/root/webapp/cpu.pprof ./pprof/profilefiles/cpu.pprof
+  rsync -e "ssh -p ${APP_HOSTS_SSH_PORT[host_idx]} -i ${APP_HOSTS_SSH_PUB_KEY[host_idx]}" -av ${APP_HOSTS_SSH_USER[host_idx]}@${APP_HOSTS[host_idx]}:/home/root/webapp/cpu.pprof ./pprof/profilefiles/cpu.pprof
 
 done
 
