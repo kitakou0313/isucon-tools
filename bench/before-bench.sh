80882d205d132c41dea26c124b38255bc3f0bee8
kitakou0313 <kitakou0313@gmail.com>
2022-05-12 11:02:01 +0900

    Enable to change path

--- bench/before-bench.sh
+++ bench/before-bench.sh
@@ -12,14 +12,14 @@ source ./hosts/hosts.txt
 for ((host_idx=0; host_idx<${FRONTEND_HOSTS_NUMS}; host_idx++));
 do
   echo "Backup log and clean log in ${FRONTEND_HOSTS[host_idx]}:${FRONTEND_HOSTS_SSH_PORT[host_idx]}"
-  ssh -p ${FRONTEND_HOSTS_SSH_PORT[host_idx]} root@${FRONTEND_HOSTS[host_idx]} 'bash -s ' < ./bench/utils/nginx-backup-clean-log.sh
+  ssh -p ${FRONTEND_HOSTS_SSH_PORT[host_idx]} -i ${FRONTEND_HOSTS_SSH_PUB_KEY[host_idx]} ${FRONTEND_HOSTS_SSH_USER[host_idx]}@${FRONTEND_HOSTS[host_idx]} 'bash -s ' < ./bench/utils/nginx-backup-clean-log.sh
 done
 
 
 for ((host_idx=0; host_idx<${DB_HOSTS_NUMS}; host_idx++));
 do
   echo "Backup log and clean log in ${DB_HOSTS[host_idx]}:${DB_HOSTS_SSH_PORT[host_idx]}"
-  ssh -p ${DB_HOSTS_SSH_PORT[host_idx]} root@${DB_HOSTS[host_idx]} 'bash -s ' < ./bench/utils/slowquery-backup-clean-log.sh
+  ssh -p ${DB_HOSTS_SSH_PORT[host_idx]} -i ${DB_HOSTS_SSH_PUB_KEY[host_idx]} ${DB_HOSTS_SSH_USER[host_idx]}@${DB_HOSTS[host_idx]} 'bash -s ' < ./bench/utils/slowquery-backup-clean-log.sh
 done
 
 echo "Finish to backup log and clean log"
