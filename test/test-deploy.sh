db21a0d93e89f34a574035d1ad86cec6c4d19381
kitakou0313 <kitakou0313@gmail.com>
2022-05-15 10:28:18 +0900

    Add test scritp for sql deploy

--- test/test-deploy.sh
+++ test/test-deploy.sh
@@ -15,11 +15,20 @@ bash ./deploy/deloy.sh
 
 for ((host_idx=0; host_idx<${APP_HOSTS_NUMS}; host_idx++));
 do
-    ssh -p ${APP_HOSTS_SSH_PORT[host_idx]} -i ${APP_HOSTS_SSH_PUB_KEY[host_idx]} ${FRONTEND_HOSTS_SSH_USER[host_idx]}@${FRONTEND_HOSTS[host_idx]} 'cat /root/webapp/main' | diff -s - ./deploy-test/sample.example
+    ssh -p ${APP_HOSTS_SSH_PORT[host_idx]} -i ${APP_HOSTS_SSH_PUB_KEY[host_idx]} ${APP_HOSTS_SSH_USER[host_idx]}@${APP_HOSTS[host_idx]} 'cat /root/webapp/main' | diff -s - ./webapp/sample-webapp/sample
     if [ $? -eq 0 ]; then
         echo "Success to deploy."
     elif [ $? -eq 1 ]; then
-        echo "Script error detected. Can't deploy-test"
+        echo "Script error detected. Can't deploy sample web app"
         exit 1
     fi
+
+    ssh -p ${APP_HOSTS_SSH_PORT[host_idx]} -i ${APP_HOSTS_SSH_PUB_KEY[host_idx]} ${APP_HOSTS_SSH_USER[host_idx]}@${APP_HOSTS[host_idx]} 'cat /root/webapp/isucon/sql/0_schema.sql' | diff -s - ./webapp/sample-webapp/sql/0_schema.sql
+    if [ $? -eq 0 ]; then
+        echo "Success to sql file."
+    elif [ $? -eq 1 ]; then
+        echo "Script error detected. Can't deploy sql file."
+        exit 1
+    fi
+
 done
