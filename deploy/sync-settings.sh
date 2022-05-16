f867609afb3d6007dd5d3eaafb1178f16f616cb7
kitakou0313 <kitakou0313@gmail.com>
2022-05-16 10:27:27 +0900

    Add comment for sync configs in app host

--- deploy/sync-settings.sh
+++ deploy/sync-settings.sh
@@ -19,4 +19,13 @@ do
   rsync -e "ssh -p ${DB_HOSTS_SSH_PORT[host_idx]} -i ${DB_HOSTS_SSH_PUB_KEY[host_idx]}" -av ./configs/my.cnf ${DB_HOSTS_SSH_USER[host_idx]}@${DB_HOSTS[host_idx]}:/etc/mysql/my.cnf
 done
 
+# Below is sample code to sync setting file in APP server.
+# Please use this to sync files, like env.sh
+
+# for ((host_idx=0; host_idx<${APP_HOSTS_NUMS}; host_idx++));
+# do
+#   echo "Send configs to ${APP_HOSTS[host_idx]}:${APP_HOSTS_SSH_PORT[host_idx]}"
+#   rsync -e "ssh -p ${APP_HOSTS_SSH_PORT[host_idx]} -i ${APP_HOSTS_SSH_PUB_KEY[host_idx]}" -av $(file path in local) ${APP_HOSTS_SSH_USER[host_idx]}@${APP_HOSTS[host_idx]}:$(file path in remote)
+# done
+
 echo "Finish to sync config files"
