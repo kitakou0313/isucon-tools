ee481ed0e1fd53c574bbc9b6bdc822fd5a131507
kitakou0313 <kitakou0313@gmail.com>
2022-05-16 10:25:04 +0900

    Fix comment

--- test/test-sync-setting.sh
+++ test/test-sync-setting.sh
@@ -28,9 +28,9 @@ for ((host_idx=0; host_idx<${DB_HOSTS_NUMS}; host_idx++));
 do
     ssh -p ${DB_HOSTS_SSH_PORT[host_idx]} -i ${DB_HOSTS_SSH_PUB_KEY[host_idx]} ${DB_HOSTS_SSH_USER[host_idx]}@${DB_HOSTS[host_idx]} 'cat /etc/mysql/my.cnf' | diff -s - ./configs/my.cnf
     if [ $? -eq 0 ]; then
-        echo "Success to sync nginx.cnf."
+        echo "Success to sync my.cnf."
     elif [ $? -eq 1 ]; then
-        echo "Script error detected. to sync nginx.cnf"
+        echo "Script error detected. to sync my.cnf"
         exit 1
     fi
 done
