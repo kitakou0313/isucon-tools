5378cfb93d343146ea6c3135df1d92955e93cbd5
kitakou0313 <kitakou0313@gmail.com>
2022-07-04 10:35:45 +0900

    Change golang profiler

--- webapp/sample-webapp/cmd/main.go
+++ webapp/sample-webapp/cmd/main.go
@@ -2,12 +2,76 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
-	"github.com/pkg/profile"
+	"github.com/felixge/fgprof"
 )
 
+// echoでのサンプル
+/*
+var stopProfile func() error
+
+// for bench start
+func benchStartHandler(c echo.Context) error {
+	profilePath := "/etc/pprof"
+
+	err := os.MkdirAll(profilePath, 0777)
+	if err != nil {
+		c.Logger().Errorf("error bench start: %s", err)
+		return c.String(
+			http.StatusInternalServerError, "Fail to create bench dir",
+		)
+	}
+
+	fn := filepath.Join(profilePath, "fgprof.pprof")
+	f, err := os.Create(fn)
+	if err != nil {
+		c.Logger().Errorf("error bench start: %s", err)
+		return c.String(
+			http.StatusInternalServerError, "Fail to create profile file",
+		)
+	}
+
+	stopProfile = fgprof.Start(f, fgprof.FormatPprof)
+	return c.String(
+		http.StatusOK, "Bench start",
+	)
+}
+
+func benchStopHandler(c echo.Context) error {
+	stopProfile()
+	return c.String(
+		http.StatusOK, "Bench stop",
+	)
+}*/
+
+var stopProfile func() error
+
+// for bench start
+func startProfile() error {
+	profilePath := "/etc/pprof"
+
+	err := os.MkdirAll(profilePath, 0777)
+	if err != nil {
+		return fmt.Errorf("Fail to create dir %w", err)
+	}
+
+	fn := filepath.Join(profilePath, "fgprof.pprof")
+	f, err := os.Create(fn)
+	if err != nil {
+		return fmt.Errorf("Fail to create profile file: %w", err)
+	}
+
+	stopProfile = fgprof.Start(f, fgprof.FormatPprof)
+	return nil
+}
+
 func main() {
-	p := profile.Start(profile.ProfilePath("./"))
+	err := startProfile()
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("Hello, isucon!")
-	p.Stop()
+	stopProfile()
 }
--- webapp/sample-webapp/go.mod
+++ webapp/sample-webapp/go.mod
@@ -2,4 +2,8 @@ module hello
 
 go 1.17
 
-require github.com/pkg/profile v1.6.0 // indirect
+require (
+	github.com/felixge/fgprof v0.9.2 // indirect
+	github.com/google/pprof v0.0.0-20211214055906-6f57359322fd // indirect
+	github.com/pkg/profile v1.6.0 // indirect
+)
