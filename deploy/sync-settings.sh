4e7cce01427202a1ddf095967d6ac8f76aba949d
kitakou0313 <kitakou0313@gmail.com>
2023-09-02 15:48:53 +0900

    Update command

--- deploy/sync-settings.sh
+++ deploy/sync-settings.sh
@@ -11,14 +11,14 @@ for ((host_idx=0; host_idx<${FRONTEND_HOSTS_NUMS}; host_idx++));
 do
   echo "Send configs to ${FRONTEND_HOSTS[host_idx]}:${FRONTEND_HOSTS_SSH_PORT[host_idx]}"
   rsync -e "ssh -p ${FRONTEND_HOSTS_SSH_PORT[host_idx]} -i ${FRONTEND_HOSTS_SSH_PRIVATE_KEY[host_idx]}" \
-  -av ./configs/nginx/ ${FRONTEND_HOSTS_SSH_USER[host_idx]}@${FRONTEND_HOSTS[host_idx]}:/etc/nginx
+  -av ./configs/nginx/ ${FRONTEND_HOSTS_SSH_USER[host_idx]}@${FRONTEND_HOSTS[host_idx]}:/tmp/nginx
 done
 
 for ((host_idx=0; host_idx<${DB_HOSTS_NUMS}; host_idx++));
 do
   echo "Send configs to ${DB_HOSTS[host_idx]}:${DB_HOSTS_SSH_PORT[host_idx]}"
   rsync -e "ssh -p ${DB_HOSTS_SSH_PORT[host_idx]} -i ${DB_HOSTS_SSH_PRIVATE_KEY[host_idx]}" \
-  -av ./configs/mysql/ ${DB_HOSTS_SSH_USER[host_idx]}@${DB_HOSTS[host_idx]}:/etc/mysql
+  -av ./configs/mysql/ ${DB_HOSTS_SSH_USER[host_idx]}@${DB_HOSTS[host_idx]}:/tmp/mysql
 done
 
 # Below is sample code to sync setting file in APP server.
