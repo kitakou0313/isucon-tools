1da4a55d16b7423eb39a76015e29ed8f098ce11a
kitakou0313 <kitakou0313@gmail.com>
2023-11-26 11:20:53 +0900

    fix: add tmux session to operate hosts

--- activate-tmux.sh
+++ activate-tmux.sh
@@ -20,8 +20,13 @@ do
   tmux new -s "app-${host_idx}" -d
 done
 
+for ((host_idx=0; host_idx<${ALL_HOSTS_NUMS}; host_idx++));
+do
+  tmux new -s "host-$((host_idx + 1))" -d
+done
+
 # topでの監視用に全hostへのSSH
-tmux new-session -s "all-hosts" -d ssh -p ${ALL_HOSTS_SSH_PORT[0]} -i ${ALL_HOSTS_SSH_PRIVATE_KEY[0]} ${ALL_HOSTS_SSH_USER[0]}@${ALL_HOSTS[0]} &&\
+tmux new-session -s "monitor-hosts" -d ssh -p ${ALL_HOSTS_SSH_PORT[0]} -i ${ALL_HOSTS_SSH_PRIVATE_KEY[0]} ${ALL_HOSTS_SSH_USER[0]}@${ALL_HOSTS[0]} &&\
 for ((host_idx=1; host_idx<${ALL_HOSTS_NUMS}; host_idx++));
 do
   tmux split-window
