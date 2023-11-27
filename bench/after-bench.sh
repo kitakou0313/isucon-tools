17c2290ad415cbe091902eb6e3e90c3e191b7b7c
kitakou0313 <kitakou0313@gmail.com>
2023-11-27 22:48:39 +0900

    fix: set timestamp

--- bench/after-bench.sh
+++ bench/after-bench.sh
@@ -10,6 +10,8 @@ source ./hosts/hosts.txt
 
 # curl http://${FRONTEND_HOSTS[0]}/api/bench/stop
 
+TIMESTAMP="$(date '+%Y%m%d_%H%M%S')"
+
 for ((host_idx=0; host_idx<${FRONTEND_HOSTS_NUMS}; host_idx++));
 do
   LOG_DIR="./alp/log/${FRONTEND_HOSTS_PATH[host_idx]}"
@@ -17,13 +19,11 @@ do
     mkdir "${LOG_DIR}"
   fi
 
-  if [ -f "${LOG_DIR}/access.log" ]; then 
-    cp "${LOG_DIR}/access.log" "${LOG_DIR}/access.log.bk.$(date '+%Y%m%d_%H%M%S')"
-  fi
+  LOG_FILE_NAME="${LOG_DIR}/access.log"
 
   echo "Fetch log from ${FRONTEND_HOSTS[host_idx]}:${FRONTEND_HOSTS_SSH_PORT[host_idx]}"
   rsync -e "ssh -p ${FRONTEND_HOSTS_SSH_PORT[host_idx]} -i ${FRONTEND_HOSTS_SSH_PRIVATE_KEY[host_idx]}" \
-  -av ${FRONTEND_HOSTS_SSH_USER[host_idx]}@${FRONTEND_HOSTS[host_idx]}:/var/log/nginx/access.log "${LOG_DIR}/access.log"
+  -av ${FRONTEND_HOSTS_SSH_USER[host_idx]}@${FRONTEND_HOSTS[host_idx]}:/var/log/nginx/access.log "${LOG_FILE_NAME}.${TIMESTAMP}"
 done
 
 for ((host_idx=0; host_idx<${DB_HOSTS_NUMS}; host_idx++));
@@ -34,13 +34,11 @@ do
   fi
 
   LOG_FILE_NAME="${LOG_DIR}/mysql-slow.log"
-  if [ -f "${LOG_FILE_NAME}" ]; then 
-    cp "${LOG_FILE_NAME}" "${LOG_FILE_NAME}.bk.$(date '+%Y%m%d_%H%M%S')"
-  fi
+
   # Fetch mysql slowquery-log
   echo "Fetch mysql slow query log from ${DB_HOSTS[host_idx]}:${DB_HOSTS_SSH_PORT[host_idx]}"
   rsync -e "ssh -p ${DB_HOSTS_SSH_PORT[host_idx]} -i ${DB_HOSTS_SSH_PRIVATE_KEY[host_idx]}" \
-  -av ${DB_HOSTS_SSH_USER[host_idx]}@${DB_HOSTS[host_idx]}:/var/log/mysql/mysql-slow.log ${LOG_FILE_NAME}
+  -av ${DB_HOSTS_SSH_USER[host_idx]}@${DB_HOSTS[host_idx]}:/var/log/mysql/mysql-slow.log "${LOG_FILE_NAME}.${TIMESTAMP}"
 done
 
 for ((host_idx=0; host_idx<${APP_HOSTS_NUMS}; host_idx++));
@@ -51,14 +49,11 @@ do
   fi
 
   LOG_FILE_NAME="${LOG_DIR}/fgprof.pprof"
-  if [ -f "${LOG_FILE_NAME}" ]; then 
-    cp "${LOG_FILE_NAME}" "${LOG_FILE_NAME}.bk.$(date '+%Y%m%d_%H%M%S')"
-  fi
 
   # Fetch pprof logs
   echo "Fetch pprof log from ${APP_HOSTS[host_idx]}:${APP_HOSTS_SSH_PORT[host_idx]}"
   rsync -e "ssh -p ${APP_HOSTS_SSH_PORT[host_idx]} -i ${APP_HOSTS_SSH_PRIVATE_KEY[host_idx]}" \
-  -av ${APP_HOSTS_SSH_USER[host_idx]}@${APP_HOSTS[host_idx]}:/tmp/pprof/fgprof.pprof "${LOG_FILE_NAME}"
+  -av ${APP_HOSTS_SSH_USER[host_idx]}@${APP_HOSTS[host_idx]}:/tmp/pprof/fgprof.pprof "${LOG_FILE_NAME}.${TIMESTAMP}"
 
 done
 
