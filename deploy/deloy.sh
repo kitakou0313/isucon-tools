9215a25671e11e553eed57741d6b60fe5028609b
kitakou0313 <kitakou0313@gmail.com>
2026-07-25 22:43:20 +0900

    add: add arrays

--- deploy/deloy.sh
+++ deploy/deloy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 # This scripts is used to 
 # - send bin and init SQL files to host with rsync
 
@@ -11,11 +11,11 @@ for ((host_idx=0; host_idx<${APP_HOSTS_NUMS}; host_idx++));
 do
   echo "Deploy to ${APP_HOSTS[host_idx]}:${APP_HOSTS_SSH_PORT[host_idx]}"
   rsync -e "ssh -p ${APP_HOSTS_SSH_PORT[host_idx]} -i ${APP_HOSTS_SSH_PRIVATE_KEY[host_idx]}" \
-  -av ./webapp/ ${APP_HOSTS_SSH_USER[host_idx]}@${APP_HOSTS[host_idx]}:${HOST1_APP_DEPLOY_DIR}
+  -av ./webapp/ ${APP_HOSTS_SSH_USER[host_idx]}@${APP_HOSTS[host_idx]}:${APP_HOSTS_APP_DEPLOY_DIR[host_idx]}
 
   ssh -t -p ${APP_HOSTS_SSH_PORT[host_idx]} -i ${APP_HOSTS_SSH_PRIVATE_KEY[host_idx]} \
   ${APP_HOSTS_SSH_USER[host_idx]}@${APP_HOSTS[host_idx]} \
-  "sudo systemctl restart ${HOST1_APP_SYSTEMCTL_SERVICE_NAME}"
+  "sudo systemctl restart ${APP_HOSTS_SYSTEMCTL_SERVICE_NAME[host_idx]}"
 done
 
 echo "Start to deploy O11y tools"
@@ -24,7 +24,7 @@ for ((host_idx=0; host_idx<${APP_HOSTS_NUMS}; host_idx++));
 do
   echo "Deploy to ${APP_HOSTS[host_idx]}:${APP_HOSTS_SSH_PORT[host_idx]}"
   rsync -e "ssh -p ${APP_HOSTS_SSH_PORT[host_idx]} -i ${APP_HOSTS_SSH_PRIVATE_KEY[host_idx]}" \
-  -av ./obi-setup ${APP_HOSTS_SSH_USER[host_idx]}@${APP_HOSTS[host_idx]}:/root/
+  -av ./obi-setup ${APP_HOSTS_SSH_USER[host_idx]}@${APP_HOSTS[host_idx]}:${APP_HOSTS_O11Y_TOOL_DEPLOY_DIR[host_idx]}
 
 done
 
